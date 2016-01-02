05d34e2e15b483c2d2b084584dea6c02c8391988
Jussi Vesala <jussi.vesala@reaktor.fi>
2016-01-02 11:59:34 +0200

    remove optional. we should have result field always

--- teknappi/teknappi/Domain.swift
+++ teknappi/teknappi/Domain.swift
@@ -10,17 +10,16 @@ import Foundation
 import Argo
 
 struct ContactRequestResult {
-    var result: String?
+    var result: String
 }
 
 extension ContactRequestResult: Decodable {
-    static func create(result: String?) -> ContactRequestResult {
+    static func create(result: String) -> ContactRequestResult {
         return ContactRequestResult(result: result)
     }
     
     static func decode(json: JSON) -> Decoded<ContactRequestResult> {
-        return ContactRequestResult.create
-            <^> json <|? "result"
+        return ContactRequestResult.create <^> json <| "result"
     }
 }
 
